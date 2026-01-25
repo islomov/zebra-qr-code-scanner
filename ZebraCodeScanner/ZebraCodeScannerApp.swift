d971782e939985c8956267faf08d504606560a55
Sardor <sardor.islomov.96@gmail.com>
2026-01-25 17:08:16 +0500

    Initial Commit

---
+++ ZebraCodeScanner/ZebraCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZebraCodeScannerApp.swift
+//  ZebraCodeScanner
+//
+//  Created by Sardor Islomov on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ZebraCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
